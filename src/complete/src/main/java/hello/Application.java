d480cd38b0f36a83e16bde512013b3c697bcd6d0
ilakkmanoharan <ilakkmanoharan@gmail.com>
2017-05-24 16:24:53 -0500

    Update Application.java

--- src/complete/src/main/java/hello/Application.java
+++ src/complete/src/main/java/hello/Application.java
@@ -45,9 +45,9 @@ public class Application {
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
 		    
-		    restTemplate.setInterceptors(Collections.singletonList(new RequestHeaderInterceptor()));
+		        restTemplate.setInterceptors(Collections.singletonList(new RequestHeaderInterceptor()));
 		    
-            //List the dashboard administrators in this organization
+                        //List the dashboard administrators in this organization
 				
 			String dashboardAdmins = restTemplate.getForObject("https://dashboard.meraki.com/api/v0/organizations/{orgId}/admins", String.class, "660707");
 			
@@ -61,22 +61,62 @@ public class Application {
 			
 			System.out.println("Networks in this organization " + networks + "\n");
 			
-            //////////////////////////////////////////////////////////////////////////////////////////////////
+                        //////////////////////////////////////////////////////////////////////////////////////////////////
 			
-            //List the devices in the network by networkId
+                        //List the devices in the network by networkId
 
-             String devices = restTemplate.getForObject("https://dashboard.meraki.com/api/v0/networks/{networkId}/devices", String.class, "N_566327653141842928");
+                        String devices = restTemplate.getForObject("https://dashboard.meraki.com/api/v0/networks/{networkId}/devices", String.class, "N_566327653141842928");
 
-             System.out.println("Devices in this network " + devices + "\n");
-			
+                        System.out.println("Devices in this network " + devices + "\n");
 			
 			/////////////////////////////////////////////////////////////////////////////////////////////////
              
-             //Display the organization details by organization id
+                       //Display the organization details by organization id
 		    
-		    String orgDetails = restTemplate.getForObject("https://dashboard.meraki.com/api/v0/organizations/{orgId}", String.class, "660707");
+		       String orgDetails = restTemplate.getForObject("https://dashboard.meraki.com/api/v0/organizations/{orgId}", String.class, "660707");
 		  
-		    System.out.println("Organization Details:   " + orgDetails + "\n");
+		       System.out.println("Organization Details:   " + orgDetails + "\n");
+			
+			/////////////////////////////////////////////////////////////////////////////////////////////////
++		    
++		       //Create a new organization
++		    
++			Organization obj = new Organization();
++			obj.setName("neworg");
++	        	HttpEntity entity = new HttpEntity(obj);
++			ResponseEntity<String> out = restTemplate.exchange("https://n6.meraki.com/api/v0/organizations", HttpMethod.POST, entity, String.class);
++		        System.out.println("result5:  " + out.getBody());
++		        System.out.println("result5:  " + out.getStatusCode());
++			
++		    /////////////////////////////////////////////////////////////////////////////////////////////////
++		    
++		    //Create a new dashboard administrator
++			
++			Map<String, String> tags = new HashMap<String, String>();
++			tags.put("tag", "west");
++			tags.put("access", "readonly");
++			
++			MultiValueMap<String, Object> adminMap = new LinkedMultiValueMap<String, Object>();
++	                adminMap.add("name", "newAdmin1");
++	                adminMap.add("email", "ilakk@Meraki.com");
++	                adminMap.add("OrgAccess", "none");
++	                adminMap.add("tags", tags);
++	      
++			
++	// send it!
+	// ResponseEntity<String> out12 = restTemplate.exchange(adminreduri, HttpMethod.POST, entity12, String.class);
++			
++	ResponseEntity<String> out12 = restTemplate.postForEntity("https://n6.meraki.com/api/v0/organizations/{orgId}/admins", adminMap, String.class, "660707");
++			
++	//ResponseEntity<String> out12 = restTemplate.exchange("https://n6.meraki.com/api/v0/organizations/{orgId}/admins", HttpMethod.POST, entity12, String.class, "660707");
++			
++.      System.out.println("result12:  " + out12.getBody());
++			  
++.      System.out.println("result12:  " + out12.getStatusCode());
++		
+ 		};
+ 	}
+ } 
 		   
 		  
 		};
